21e7db0fe9bc06baa069f488c2e3d34895d47e1f
nadunindunil <nadunindunil@users.noreply.github.com>
2020-07-12 19:46:03 +0000

    deploy: edbcd11f61b801de345eb1138d89ab9c3e21dc6f

---
+++ precache-manifest.f902c0cf248ebee4d72b614c29b669e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f80c18a61bd02f8ba80d8d0289a01730",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c11aed6f2309e3f2f2a5",
+    "url": "/static/css/main.54b687de.chunk.css"
+  },
+  {
+    "revision": "2f4e69a50cf34bbee63c",
+    "url": "/static/js/2.8a82db27.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8a82db27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c11aed6f2309e3f2f2a5",
+    "url": "/static/js/main.ec834182.chunk.js"
+  },
+  {
+    "revision": "52b9db38f75994e686a6",
+    "url": "/static/js/runtime-main.1b29a7cc.js"
+  }
+]);
