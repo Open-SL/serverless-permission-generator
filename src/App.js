40a1fe7a0b4c9c97cb4c37efd7789e591dbfc03b
sachintha97 <ssamarasiri12@gmail.com>
2020-07-12 13:04:02 +0530

    initial commit :rocket:

--- src/App.js
+++ src/App.js
@@ -1,24 +1,217 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import logo from './logo.svg';
+import "./App.css";
+import {
+  FormControl,
+  TextField,
+  RadioGroup,
+  Radio,
+  FormControlLabel,
+  Switch,
+  Typography,
+  Grid,
+  Button,
+  Box,
+} from "@material-ui/core";
+
+import GetAppIcon from "@material-ui/icons/GetApp";
 
 function App() {
+  const [state, setState] = React.useState({
+    checkedA: true,
+    checkedB: true,
+  });
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Serverless Permission Policy Generator
       </header>
+      <Grid container>
+        <Grid item xs={6}>
+          <FormControl>
+            <Box m={2}>
+              <Typography variant="h5" component="h2">
+                Project Meta Data
+              </Typography>
+
+              <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+                spacing="1"
+              >
+                <Grid item xs>
+                  <Typography variant="h6"> Project Name </Typography>
+                </Grid>
+                <Grid item xs>
+                  <TextField
+                    id="standard-search"
+                    InputLabelProps={{ shrink: false }}
+                  />
+                </Grid>
+              </Grid>
+
+              <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+                spacing="1"
+              >
+                <Grid item xs>
+                  <Typography variant="h6"> Group </Typography>
+                </Grid>
+                <Grid item xs>
+                  <TextField
+                    id="standard-search"
+                    InputLabelProps={{ shrink: false }}
+                  />
+                </Grid>
+              </Grid>
+
+              <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+                spacing="1"
+              >
+                <Grid item xs>
+                  <Typography variant="h6"> Role Name</Typography>
+                </Grid>
+                <Grid item xs>
+                  <TextField
+                    id="standard-search"
+                    InputLabelProps={{ shrink: false }}
+                  />
+                </Grid>
+              </Grid>
+            </Box>
+            {/* Second Part */}
+            <Box m={2}>
+              <Typography variant="h5" component="h2">
+                Project Dependencies
+              </Typography>
+
+              <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+                spacing="1"
+              >
+                <Grid item>
+                  <Typography component="div">
+                    <Typography variant="h6"> Test Button 1</Typography>
+                    <Grid
+                      component="label"
+                      container
+                      alignItems="center"
+                      spacing={1}
+                    >
+                      <Grid item>Off</Grid>
+                      <Grid item>
+                        <FormControlLabel
+                          control={
+                            <Switch
+                              checked={state.checkedA}
+                              onChange={handleChange}
+                              name="checkedA"
+                            />
+                          }
+                        />
+                      </Grid>
+                      <Grid item>On</Grid>
+                    </Grid>
+                  </Typography>
+                </Grid>
+              </Grid>
+
+              {/* first radio button field */}
+              <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+                spacing="1"
+              >
+                <Grid item xs>
+                  <Typography variant="h6"> Packaging </Typography>
+                </Grid>
+
+                <RadioGroup
+                  aria-label="Packaging"
+                  name="Packaging1"
+                  onChange={handleChange}
+                >
+                  <Grid item>
+                    <FormControlLabel
+                      value="test1"
+                      control={<Radio />}
+                      label="test1"
+                    />
+                    <FormControlLabel
+                      value="test2"
+                      control={<Radio />}
+                      label="test2"
+                    />
+                  </Grid>
+                </RadioGroup>
+              </Grid>
+
+              {/* second radio button field */}
+              <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+                spacing="1"
+              >
+                <Grid item xs>
+                  <Typography variant="h6"> Version </Typography>
+                </Grid>
+
+                <RadioGroup
+                  aria-label="version"
+                  name="version1"
+                  onChange={handleChange}
+                >
+                  <Grid item>
+                    <FormControlLabel
+                      value="test1"
+                      control={<Radio />}
+                      label="test1"
+                    />
+                    <FormControlLabel
+                      value="test2"
+                      control={<Radio />}
+                      label="test2"
+                    />
+                  </Grid>
+                </RadioGroup>
+              </Grid>
+
+              <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                startIcon={<GetAppIcon />}
+              >
+                Generate
+              </Button>
+            </Box>
+          </FormControl>
+        </Grid>
+        <Grid item xs={6}>
+          12
+        </Grid>
+      </Grid>
     </div>
   );
 }
