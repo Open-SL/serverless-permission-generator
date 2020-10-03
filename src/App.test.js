273d8054fd40ba945786d1272719f6ce9d764fcf
Nitkalya Wiriyanuparb <dekpient@gmail.com>
2020-10-03 19:37:45 +1300

    Add initial tests for App and generator

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor, getNodeText } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('display the generated policy for the given project data', async () => {
+    const { container } = render(<App />);
+
+    userEvent.type(container.querySelector('input[name="projectName"]'), 'hacktoberfest');
+    userEvent.type(container.querySelector('input[name="accountId"]'), '123457891234');
+    userEvent.type(container.querySelector('input[name="region"]'), 'us-east-1');
+    userEvent.type(container.querySelector('input[name="stage"]'), 'dev');
+    userEvent.click(container.querySelector('button[type="button"]'));
+
+    await waitFor(() => {
+      expect(getNodeText(container.querySelector('.string-value'))).toEqual('"2012-10-17"');
+    });
+});
+
+test('display error for invalid account ID', async () => {
+    const { container, getByText } = render(<App />);
+
+    userEvent.type(container.querySelector('input[name="projectName"]'), 'hacktoberfest');
+    userEvent.type(container.querySelector('input[name="accountId"]'), 'boo');
+    userEvent.type(container.querySelector('input[name="region"]'), 'us-east-1');
+    userEvent.type(container.querySelector('input[name="stage"]'), 'dev');
+    userEvent.click(container.querySelector('button[type="button"]'));
+
+    await waitFor(() => {
+      expect(getByText('Invalid ID')).toBeInTheDocument();
+      expect(container.querySelector('.string-value')).not.toBeInTheDocument();
+    });
 });
---
+++ src/generator/__test__/index.test.js
@@ -0,0 +1,110 @@
+import generator from "..";
+
+test('generating minimum policy', () => {
+  const region = 'us-east-1';
+  const projectName = 'hacktoberfest';
+  const stage = 'test';
+  const accountId = '123456789098';
+
+  expect(generator({ region, projectName, stage, accountId })).toEqual({
+    Version: '2012-10-17',
+    Statement: [
+      {
+        Effect: 'Allow',
+        Action: ['cloudformation:List*', 'cloudformation:Get*', 'cloudformation:ValidateTemplate'],
+        Resource: ['*'],
+      },
+      {
+        Effect: 'Allow',
+        Action: [
+          'cloudformation:CreateStack',
+          'cloudformation:CreateUploadBucket',
+          'cloudformation:DeleteStack',
+          'cloudformation:Describe*',
+          'cloudformation:UpdateStack',
+        ],
+        Resource: [`arn:aws:cloudformation:${region}:${accountId}:stack/${projectName}-${stage}/*`],
+      },
+      {
+        Effect: 'Allow',
+        Action: ['lambda:Get*', 'lambda:List*', 'lambda:CreateFunction'],
+        Resource: ['*'],
+      },
+      {
+        Effect: 'Allow',
+        Action: [
+          's3:GetBucketLocation',
+          's3:CreateBucket',
+          's3:DeleteBucket',
+          's3:ListBucket',
+          's3:GetBucketPolicy',
+          's3:PutBucketPolicy',
+          's3:ListBucketVersions',
+          's3:PutAccelerateConfiguration',
+          's3:GetEncryptionConfiguration',
+          's3:PutEncryptionConfiguration',
+        ],
+        Resource: [`arn:aws:s3:::${projectName}*serverlessdeploy*`],
+      },
+      {
+        Effect: 'Allow',
+        Action: ['s3:PutObject', 's3:GetObject', 's3:DeleteObject'],
+        Resource: [`arn:aws:s3:::${projectName}*serverlessdeploy*`],
+      },
+      {
+        Effect: 'Allow',
+        Action: [
+          'lambda:AddPermission',
+          'lambda:CreateAlias',
+          'lambda:DeleteFunction',
+          'lambda:InvokeFunction',
+          'lambda:PublishVersion',
+          'lambda:RemovePermission',
+          'lambda:Update*',
+        ],
+        Resource: [`arn:aws:lambda:${region}:${accountId}:function:${projectName}-${stage}-*`],
+      },
+
+      {
+        Effect: 'Allow',
+        Action: ['cloudwatch:GetMetricStatistics'],
+        Resource: ['*'],
+      },
+      {
+        Action: ['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:DeleteLogGroup'],
+        Resource: [`arn:aws:logs:${region}:${accountId}:*`],
+        Effect: 'Allow',
+      },
+      {
+        Action: ['logs:PutLogEvents'],
+        Resource: [`arn:aws:logs:${region}:${accountId}:*`],
+        Effect: 'Allow',
+      },
+      {
+        Effect: 'Allow',
+        Action: ['logs:DescribeLogStreams', 'logs:DescribeLogGroups', 'logs:FilterLogEvents'],
+        Resource: ['*'],
+      },
+      {
+        Effect: 'Allow',
+        Action: ['events:Put*', 'events:Remove*', 'events:Delete*'],
+        Resource: [`arn:aws:events:${region}:${accountId}:rule/${projectName}-${stage}-${region}`],
+      },
+      {
+        Effect: 'Allow',
+        Action: ['events:DescribeRule'],
+        Resource: [`arn:aws:events:${region}:${accountId}:rule/${projectName}-${stage}-*`],
+      },
+      {
+        Effect: 'Allow',
+        Action: ['iam:PassRole'],
+        Resource: [`arn:aws:iam::${accountId}:role/*`],
+      },
+      {
+        Effect: 'Allow',
+        Action: ['iam:GetRole', 'iam:CreateRole', 'iam:PutRolePolicy', 'iam:DeleteRolePolicy', 'iam:DeleteRole'],
+        Resource: [`arn:aws:iam::${accountId}:role/${projectName}-${stage}-${region}-lambdaRole`],
+      }
+    ]
+  });
+});
